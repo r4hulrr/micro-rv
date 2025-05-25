cf7d817d57cc6510b7ca4b7c50d6273a11ce1d85
Rahul <rahulram07r@gmail.com>
2025-05-25 16:46:08 +1000

    fixed register file and scrapped individual registers - simpler this way

--- code/register_file.vhdl
+++ code/register_file.vhdl
@@ -4,10 +4,10 @@ use ieee.numeric_std.all;
 
 entity reg_file is
 	port(
-		clk, rd_en, wr_en: in std_logic_vector;			-- clk, read enable and write enable signals
-		sel: in std_logic_vector (4 downto 0);			-- to choose which of the 32 registers to read/write to 
-		data_in: in std_logic_vector (31 downto 0);		-- data from memory if writing to register
-		data_out: out std_logic_vector (31 downto 0)	-- data from one particular register
+		clk, wr_en: in std_logic;										-- clk, write enable signals
+		rd_sel1, rd_sel2, rw_sel: in std_logic_vector (4 downto 0);		-- to choose which of the 32 registers to read/write to 
+		data_in: in std_logic_vector (31 downto 0);						-- data in from memory if writing to register
+		rd1, rd2: out std_logic_vector (31 downto 0)					-- data out from selected registers
 	);
 end reg_file;
 
@@ -19,10 +19,10 @@ begin
 	begin
 		if rising_edge(clk) then
 			if wr_en = '1' then
-				reg_file(to_integer(unsigned(sel))) <= data_in;
-			elsif rd_en = '1' then
-				data_out <= reg_file(to_integer(unsigned(sel)));
+				reg_file(to_integer(unsigned(rw_sel))) <= data_in;		-- multiplexer to select the register
 			end if;
 		end if;
 	end process;
+	rd1 <= reg_file(to_integer(unsigned(rd_sel1)));
+	rd2 <= reg_file(to_integer(unsigned(rd_sel2)));
 end reg_file_arch;

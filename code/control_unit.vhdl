e43a43b7d3de6bb7e0542347b74508a9793ff792
Rahul <rahulram07r@gmail.com>
2025-05-28 02:25:11 +1000

    memory and load instructions partially implemented in control unit

--- code/control_unit.vhdl
+++ code/control_unit.vhdl
@@ -11,28 +11,42 @@ entity ctrl_unit is
         f7: in std_logic_vector(6 downto 0);
         imm: in std_logic_vector(31 downto 0);
         alu_op: out std_logic_vector (3 downto 0);
-        ram_addr: out std_logic_vector(9 downto 0)		-- address in ram to read/write
+        alu_a, alu_b: out std_logic_vector(31 downto 0)		-- alu operands
     );
 end ctrl_unit;
 
 architecture ctrl_unit_arch of ctrl_unit is
+    -- signals for the register file
+    signal rf_wr_en: std_logic;										-- clk, write enable signals
+    signal rf_rd_sel1, rf_rd_sel2, rf_rw_sel: std_logic_vector (4 downto 0);		-- to choose which of the 32 registers to read/write to 
+    signal rf_data_in: std_logic_vector (31 downto 0);						-- data in from memory if writing to register
+    signal rf_rd1, rf_rd2: std_logic_vector (31 downto 0);					-- data out from selected registers
 begin
+    -- instantiate the register file
     register_file: entity work.reg_file(reg_file_arch)
-        port map(clk=>clk,wr_en=>alu_wr_en)
-    -- process to decode the address to input to 
+        port map(
+            clk=>clk,
+            wr_en=>rf_wr_en,
+            rd_sel1=>rf_rd_sel1,
+            rd_sel2=>rf_rd_sel2,
+            rw_sel=>rf_rw_sel,
+            data_in=>rf_data_in,
+            rd1=>rf_rd1,
+            rd2=>rf_rd2
+            );
+    -- process for memory store and load instructions
     mem_decode: process (opcode,rs1,rs2,rd,f3,f7,imm)
     begin
-        if opcode = "0000011" then
-            case f3 is 
-                when "000" =>
-                    
-            end case;
+        if opcode = "0000011" or opcode = "0100011" then  
+            alu_op <= "0000";           -- add as the value in rs1 should be added to immediate
+            rf_rd_sel1 <= rs1;          -- to get the value of rs1 register from register file
         end if;
     end process mem_decode;
-    -- process to decode the alu opcode required as input to the alu
+    -- process for register-register instructions and register-immediate instructions 
     alu_decode: process(opcode,rs1,rs2,rd,f3,f7,imm)
     begin
         if opcode = "0110011" or opcode = "0010011" then
+            rf_rd_sel1 <= rs1;          -- to get the value of rs1 register from register file
             case f3 is
                 when "000" =>
                     if f7 = "0100000" then

3578569b596d0eb89807085d2099c30e357ebe42
Rahul Ramachandran <rahulram07r@gmail.com>
2025-06-28 01:11:22 +1000

    updated pc with new architecture

--- code/pc.vhdl
+++ code/pc.vhdl
@@ -4,27 +4,20 @@ use ieee.numeric_std.all;
 
 entity pc is
     port(
-        clk, reset: in std_logic;
-        pc_op: in std_logic_vector(1 downto 0);            
-        addr_in: in std_logic_vector(7 downto 0); 
-        addr: out std_logic_vector(7 downto 0)
+        i_clk, i_reset: in std_logic;        
+        i_addr: in std_logic_vector(7 downto 0); 
+        o_addr: out std_logic_vector(7 downto 0)
     );
 end pc;
 
 architecture pc_arch of pc is
-    signal pc_addr: std_logic_vector(7 downto 0);
 begin
     process(clk, reset)
     begin
         if reset = '1' then
-            pc_addr <= x"00";
+            o_addr <= (others => '0');
         elsif rising_edge(clk) then
-            if pc_op = "01" then
-                pc_addr <= std_logic_vector(unsigned(pc_addr)+1);
-            elsif pc_op = "10" then
-                pc_addr <= addr_in;
-            end if;
+            o_addr <= i_addr;
         end if;
     end process;
-    addr <= pc_addr;
 end pc_arch;

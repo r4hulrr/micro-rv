d11215bf142988e851c509beccedca6bd255a0b9
Rahul Ramachandran <rahulram07r@gmail.com>
2025-06-28 19:03:24 +1000

    updated ram code

--- code/ram.vhdl
+++ code/ram.vhdl
@@ -4,25 +4,37 @@ use ieee.numeric_std.all;
 
 entity ram is
 	port(
-		clk,wr_en,rd_en: in std_logic;				-- clk, read enable and write enable
-		addr: in std_logic_vector(9 downto 0);		-- address in ram to read/write
-		data_in: in std_logic_vector(31 downto 0);	-- data to write to register
-		data_out: out std_logic_vector(31 downto 0)	-- data read from register
+		i_clk	: in std_logic;				-- clk, reset and write enable
+		i_reset	: in std_logic;
+		i_wr_en	: in std_logic;
+		i_addr	: in std_logic_vector(9 downto 0);		-- address in ram to read/write
+		i_data	: in std_logic_vector(31 downto 0);	-- data to write to register
+		o_data	: out std_logic_vector(31 downto 0)	-- data read from register
 	);
 end ram;
 
 architecture ram_arch of ram is
 	type ram_type is array (1023 downto 0) of std_logic_vector(31 downto 0);	--4kB of RAM
-	signal ram_array: ram_type;
+	signal sig_ram_array: ram_type;
 begin
-	process(clk)
+	process(i_clk, 
+			i_reset,
+			i_wr_en,
+			i_addr,
+			i_wr_en) 
+		variable var_ram_array	: ram_type;
+		variable var_ram_addr	: integer;
 	begin
-		if rising_edge(clk) then
-			if rd_en = '1' then
-				data_out <= ram_array(to_integer(unsigned(addr)));
-			elsif wr_en = '1' then
-				ram_array(to_integer(unsigned(addr))) <= data_in;
+		variable var_ram_addr := to_integer(i_addr);
+		if i_reset = '1' then
+			var_ram_array => (others => (others=> "0"));
+		elsif falling_edge(i_clk) then
+			if wr_en = '1' then
+				var_ram_array(var_ram_addr) := i_data;
 			end if;
 		end if;
+		o_data <= var_ram_array(var_ram_addr);
+
+		sig_ram_array <= var_ram_array;
 	end process;
 end ram_arch;
